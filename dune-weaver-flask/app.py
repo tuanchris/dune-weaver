d6189973cb4eb864a88090d4824bc496365188aa
Fabio De Simone <prototypefab@gmail.com>
2025-01-19 17:12:59 +0100

    disable debug flag

--- dune-weaver-flask/app.py
+++ dune-weaver-flask/app.py
@@ -444,4 +444,4 @@ def api_delete_theta_rho_file():
 if __name__ == '__main__':
     # Auto-connect to serial
     connect_to_serial()
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0', port=8080)
